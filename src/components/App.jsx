1914af8abf26d14fc158319fc7907a8cbf635a85
Anastasia Carlii <nastjaorosszul@gmail.com>
2023-08-07 00:43:02 +0100

    second

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 import { Profile } from './Profile/Profile';
-// import { Statistics } from './Statistics/Statistics';
+import { Statistics } from './Statistics/Statistics';
 // import { FriendList } from './FriendsList/FriendsList';
 // import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 import user from './Profile/user.json';
-// import data from './Statistics/data.json';
+import data from './Statistics/data.json';
 // import friends from './FriendsList/friends.json';
 // import transactions from './TransactionHistory/transactions.json';
 
@@ -21,8 +21,8 @@ export const App = () => {
     // }}
     >
       <Profile user={user} />
-      {/* <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
+      <Statistics title="Upload stats" stats={data} />
+      {/* <FriendList friends={friends} />
       <TransactionHistory items={transactions} /> */}
     </div>
   );
